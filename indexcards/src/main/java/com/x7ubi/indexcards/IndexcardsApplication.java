c64c6fb83cc0e49cec14e04e5094ca986cde3fbc
Tobias Renner <tobi.renner03@gmail.com>
2022-11-19 10:38:47 +0100

    Initial commit

---
+++ indexcards/src/main/java/com/x7ubi/indexcards/IndexcardsApplication.java
@@ -0,0 +1,13 @@
+package com.x7ubi.indexcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexcardsApplication.class, args);
+	}
+
+}
