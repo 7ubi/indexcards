cf5b4bb7611da522189560174b019acecae5c402
tobias <tobi.renner03@gmail.com>
2023-02-19 12:35:21 +0100

    Form Layout

--- indexcards-ui/src/app/component/indexcard/create-indexcard/create-indexcard.component.ts
+++ indexcards-ui/src/app/component/indexcard/create-indexcard/create-indexcard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-create-indexcard',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class CreateIndexcardComponent {
 
+  createIndexcard(indexcard: NgForm) {
+
+  }
 }
