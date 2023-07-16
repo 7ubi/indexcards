bd3cc7fb7adfa435af82bcdf46214f405426cd85
Tobias Renner <tobi.renner03@gmail.com>
2023-07-16 22:23:44 +0200

    fix: fixed linting

--- indexcards-ui/src/app/component/indexcard/assessment-button/assessment-button.component.ts
+++ indexcards-ui/src/app/component/indexcard/assessment-button/assessment-button.component.ts
@@ -6,5 +6,5 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./assessment-button.component.css']
 })
 export class AssessmentButtonComponent {
-  @Input() assessment: string = '';
+  @Input() assessment = '';
 }
