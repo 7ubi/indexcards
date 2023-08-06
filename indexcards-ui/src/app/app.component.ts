c77352ba88fccbb0b41cd8c7a5cc03a29cb2e575
Tobias Renner <tobi.renner03@gmail.com>
2023-08-06 21:41:55 +0200

    feat: imported npx translation and translated
    Translated Login Component to English and German

--- indexcards-ui/src/app/app.component.ts
+++ indexcards-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'indexcards-ui';
+  lang: string = "en";
+
+  constructor(
+    public translate: TranslateService,
+  ) {
+    translate.addLangs(['en', 'de']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    let lang = browserLang?.match(/en|de/) ? browserLang : 'en';
+    this.changeLang(lang);
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
